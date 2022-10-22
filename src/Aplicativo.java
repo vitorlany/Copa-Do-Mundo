4786c405e544ed28968d49448c2ae2853f236cf6
Vitor Lany <vitorlany99@gmail.com>
2022-10-22 12:07:45 -0300

    Começo da implementação

--- src/Aplicativo.java
+++ src/Aplicativo.java
@@ -1,7 +1,20 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Aplicativo {
-    FilaCircular filaJogos = new FilaCircular(100);
+    public static FilaCircular filaJogos = new FilaCircular(100);
 
     public static void main(String[] args) {
-    
+        try {
+            File arquivo = new File("./src/tmp/partidas.txt");
+            Scanner lerArquivo = new Scanner(arquivo);
+            while (lerArquivo.hasNextLine()) {
+                Jogo novoJogo = new Jogo();
+                filaJogos.adicionarFila(novoJogo);
+            }
+            lerArquivo.close();
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+        }
     }
 }
